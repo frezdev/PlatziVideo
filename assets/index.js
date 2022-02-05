d808a701cd7599abd9c1835b43f6642e58cb1d1b
Carlos Florez <carlos.andres.ff2912@gmail.com>
2022-02-04 19:07:53 -0500

    modifique el codigo para utilizar Getters y Setters

--- assets/index.js
+++ assets/index.js
@@ -5,7 +5,7 @@ const video = document.querySelector('video');
 const player = new MediaPlayer({ 
   el: video, 
   plugins: [
-    // new AutoPlay()
+    new AutoPlay()
   ]
 });
 
--- plugins/AutoPlay.js
+++ plugins/AutoPlay.js
@@ -2,7 +2,9 @@ function AutoPlay() {
   
 }
 AutoPlay.prototype.run = function(player) {
-  player.mute();
+  if (!player.muted) {
+    player.muted = true;
+  }
   player.play();
 }
 
