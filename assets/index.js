560d0544a687d25fe6dd5ca2b5bc343bd242037f
Carlos Florez <carlos.andres.ff2912@gmail.com>
2022-02-11 03:10:00 -0500

    agregue un nuevo plugin y modifique algunos archivos

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,14 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from '../plugins/AutoPlay.js';
+import AutoPause from '../plugins/AutoPause.js';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({ 
   el: video, 
   plugins: [
-    new AutoPlay()
-  ]
+    new AutoPlay(),
+    new AutoPause()
+  ],
 });
 
 const buttonPlay = document.querySelector('.play');
@@ -14,3 +16,10 @@ buttonPlay.onclick = () => player.togglePlay();
 
 const buttonMute = document.querySelector('.mute');
 buttonMute.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .catch(error => {
+      console.log(error.message)
+    })
+}
